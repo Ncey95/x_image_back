9413aa90915d48d0336386b748b0185d75494a40
Ncey95 <the_somme@163.com>
2025-10-18 13:29:00 +0800

    初始配置1.1

--- src/main/java/com/ncey95/x_image_back/XImageBackApplication.java
+++ src/main/java/com/ncey95/x_image_back/XImageBackApplication.java
@@ -1,8 +1,11 @@
 package com.ncey95.x_image_back;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.ncey95.x_image_back.mapper")
 @SpringBootApplication
 public class XImageBackApplication {
 
