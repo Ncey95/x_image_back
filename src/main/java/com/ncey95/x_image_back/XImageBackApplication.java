c443d6c24cfd8fac2ab4e06a7fc4398fb084b16d
Ncey95 <the_somme@163.com>
2025-10-18 13:47:34 +0800

    初始配置1.2

--- src/main/java/com/ncey95/x_image_back/XImageBackApplication.java
+++ src/main/java/com/ncey95/x_image_back/XImageBackApplication.java
@@ -4,9 +4,12 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan("com.ncey95.x_image_back.mapper")
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)// 开启AOP代理，暴露代理对象，用于在切面中获取代理对象
 public class XImageBackApplication {
 
     public static void main(String[] args) {
