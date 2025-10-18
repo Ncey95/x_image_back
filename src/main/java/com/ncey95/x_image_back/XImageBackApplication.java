1a88fb82105df59843f59fcd3bbbfd766351245c
Ncey95 <the_somme@163.com>
2025-10-18 12:52:58 +0800

    初始配置

---
+++ src/main/java/com/ncey95/x_image_back/XImageBackApplication.java
@@ -0,0 +1,13 @@
+package com.ncey95.x_image_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XImageBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XImageBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ncey95/x_image_back/XImageBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncey95.x_image_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XImageBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
